f2d8020cec17e1f86d9befef5ca6fdb934021a97
ArturoAmaya <a1amaya@ucsd.edu>
2022-03-21 13:06:43 -0700

    correctly clicked to next page

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://ucsd.libcal.com/reserve');
-  await page.screenshot({ path: 'landedatpage.png' });
+  await page.screenshot({ path: './screenshots/landedatpage.png' });
 
   // Try to click the next day button
   const nextpage = await page.$('#eq-time-grid > div.fc-header-toolbar.fc-toolbar.fc-toolbar-ltr > div:nth-child(1) > div > button.fc-next-button.btn.btn-default.btn-sm');
-  //const nextpageJSON = 
-  await nextpage.screenshot({path: 'correctbutton.png'});
-  //console.log(nextpageJSON);
+  // not used at the moment, but this is to get the next button "text". there is no text its an image thing.
+  //const nextpagepropertyInnerText = await page.$eval('#eq-time-grid > div.fc-header-toolbar.fc-toolbar.fc-toolbar-ltr > div:nth-child(1) > div > button.fc-next-button.btn.btn-default.btn-sm', el => el.innerText);
+  await nextpage.screenshot({path: './screenshots/correctbutton.png'});
+  await nextpage.click();
+  await page.screenshot({path: './screenshots/nextday.png'});
 
   await browser.close();
 })();
