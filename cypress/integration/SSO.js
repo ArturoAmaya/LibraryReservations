dc23d16be5ed384edf17b67e13c066aee1cdc48c
ArturoAmaya <a1amaya@ucsd.edu>
2022-03-21 12:39:04 -0700

    reboot

--- cypress/integration/SSO.js
+++
@@ -1,6 +0,0 @@
-
-describe("Test the geisel page", {timeout: 100000}, () => {
-    it("Opens the page", ()=>{
-        cy.visit("https://a5.ucsd.edu/tritON/profile/SAML2/Redirect/SSO?execution=e3s1");
-    });
-});
