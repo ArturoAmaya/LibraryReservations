60f4c6036adad80a652e83132c380a4dc282800d
ArturoAmaya <a1amaya@ucsd.edu>
2022-03-22 12:15:20 -0700

    now works for a single reservation at a specific time. walks through from page load to reservation confirmation. THROUGH DUO

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const puppeteer = require('puppeteer');
+const args = process.argv.slice(2);
+const debug = parseInt(args[0]);
+const username = args[1];
+const password = args[2];
+
 
 (async () => {
+    // 1. LAUNCH BROWSER
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://ucsd.libcal.com/reserve', {waitUntil: 'networkidle0'});
-    await page.screenshot({ path: './screenshots/landedatpage.png' });
 
+    // 2. GO TO PAGE
+    await page.goto('https://ucsd.libcal.com/reserve', {waitUntil: 'networkidle0'});
+    if(debug){
+        await page.screenshot({ path: './screenshots/landedatpage.png' });
+    }
+    // 3. NAVIGATE TO 2 WEEKS FROM NOW
     // Try to click the next day button
     const nextpage = await page.$('#eq-time-grid > div.fc-header-toolbar.fc-toolbar.fc-toolbar-ltr > div:nth-child(1) > div > button.fc-next-button.btn.btn-default.btn-sm');
     // not used at the moment, but this is to get the next button "text". there is no text its an image thing.
@@ -16,40 +26,54 @@ const puppeteer = require('puppeteer');
     await nextpage.click();
     await page.screenshot({path: './screenshots/day3.png'});
     */
-    await page.screenshot({path: './screenshots/day0.png'});
+    if(debug){
+        await page.screenshot({path: './screenshots/day0.png'});
+    }
     for (let i =0; i<14; i++){
         await nextpage.click();
-        await page.screenshot({path: `./screenshots/day${i+1}.png`});
+        if(debug){
+            await page.screenshot({path: `./screenshots/day${i+1}.png`});
+        }
     }
 
+    // 4. REQUEST TIMESLOT(S)
     // Now you're on the correct day, two weeks from today
-    const timeslot1 = await page.$('#eq-time-grid > div.fc-view-harness.fc-view-harness-passive > div > table > tbody > tr > td:nth-child(3) > div > div > div > table > tbody > tr:nth-child(33) > td > div > div.fc-timeline-events.fc-scrollgrid-sync-inner > div:nth-child(17) > a');
+    const timeslot1 = await page.$('#eq-time-grid > div.fc-view-harness.fc-view-harness-passive > div > table > tbody > tr > td:nth-child(3) > div > div > div > table > tbody > tr:nth-child(33) > td > div > div.fc-timeline-events.fc-scrollgrid-sync-inner > div:nth-child(20) > a');
     await timeslot1.click();
 
     await page.waitForTimeout(3000);
 
+    // 5. SUBMIT REQUESTED TIME(S)
     await Promise.all([
         page.waitForNavigation({waitUntil: 'networkidle0'}),
         page.click('#submit_times')
     ]);
 
+    // 6. SINGLE SIGN-ON
     await page.waitForSelector('#main-content > div > section > h1 > span');
-    await page.screenshot({path: './screenshots/sso.png'});
+    if (debug){
+        await page.screenshot({path: './screenshots/sso.png'});
+    }
+    await page.type('#ssousername', username);
+    await page.type('#ssopassword', password);
 
-    await page.type('#ssousername', 'a1amaya');
-    await page.type('#ssopassword', 'R@D@r2d2R@D@R@D@');
-
-    await page.screenshot({path: './screenshots/ssofilledout.png'});
+    if (debug) {
+        await page.screenshot({path: './screenshots/ssofilledout.png'});
+    }
 
     await Promise.all([
         page.waitForNavigation({waitUntil: 'networkidle0'}),
         page.click('#login > button')
     ]);
 
-    await page.screenshot({path: './screenshots/ssosubmitted.png'});
-
+    if(debug) {
+        await page.screenshot({path: './screenshots/ssosubmitted.png'});
+    }
+    // 7. DUO AUTHENTICATION
     const duo = await page.$('#duo_iframe');
-    await duo.screenshot({path:'./screenshots/duocontent.png'});
+    if (debug) {
+        await duo.screenshot({path:'./screenshots/duocontent.png'});
+    }
     const duoframe = await duo.contentFrame();
 
     //await duoframe.screenshot({path:'./screenshots/duoframecontent.png'});
@@ -60,7 +84,28 @@ const puppeteer = require('puppeteer');
         duoframe.click('#auth_methods > fieldset:nth-child(1) > div.row-label.push-label > button'),
     ]);
 
+    if (debug){
     await page.screenshot({path: './screenshots/postduo.png'});
+    }
+    // 8. CONFIRM RESERVATION
 
+    // OK at this point you should be past sso and duo.
+    // wait for the content to load and then select it
+    await page.waitForSelector('#bform-terms-container > div.s-lc-eq-co-terms > div > p:nth-child(2) > strong > span > span > a'); //waiting for the UCSD library reservation policies link before proceeding
+    const confirmbtn = await page.$('#terms_accept');
+    await confirmbtn.click();
+
+    if (debug) {
+        await page.screenshot({path: './screenshots/lastconfirmpage.png'});
+    }
+    const submitbtn = await page.$('#s-lc-eq-bform-submit');
+    if (debug){
+        await submitbtn.screenshot({path: './screenshots/finalsubmitbtn.png'});
+    }
+    await submitbtn.click();
+
+    if(debug){
+        await page.screenshot({path: './screenshots/confirmationticket.png'});
+    }
     await browser.close();
 })();
